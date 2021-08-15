532fb6cf29c0e31d6dca9f470b29555403d72896
jitendranarang2610 <jitendra.narang2610@gmail.com>
2021-08-15 19:55:20 +1000

    Update CheckWeather.py

--- CheckWeather.py
+++ CheckWeather.py
@@ -1,51 +1,28 @@
-import json
-import os
+import configparser
 import requests
-
-from flask import Flask
-from flask import request
-from flask import make_response
-
-# Flask app should start in global layout
-app = Flask(__name__)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    #print(json.dumps(req, indent=4))
-    
-    res = makeResponse(req)
-    
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeResponse(req):
-    if req.get('result').get('action') != 'fetchWeatherForecast':
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    date = parameters.get("date")
-    if city is None:
-        return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
-    json_object = r.json()
-    weather=json_object['list']
-    for i in range(0,30):
-        if date in weather[i]['dt_txt']:
-            condition= weather[i]['weather'][0]['description']
-            break
-    speech = "The forecast for"+city+ "for "+date+" is "+condition
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
-    }
-
+import sys
+ 
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+ 
+def get_weather(api_key, location):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
+    r = requests.get(url)
+    return r.json()
+ 
+def main():
+    if len(sys.argv) != 2:
+        exit("Usage: {} LOCATION".format(sys.argv[0]))
+    location = sys.argv[1]
+ 
+    api_key = get_api_key()
+    weather = get_weather(api_key, location)
+ 
+    print(weather['main']['temp'])
+    print(weather)
+ 
+ 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+    main()
