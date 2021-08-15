0078677cf5b18efb8fa811fafcd425253425c960
jitendranarang2610 <jitendra.narang2610@gmail.com>
2021-08-15 22:21:18 +1000

    Update CheckWeather.py

--- CheckWeather.py
+++ CheckWeather.py
@@ -33,6 +33,7 @@ def index():
 
 
 if __name__ == '__main__':
+    print("Main method pre-entry")
     port = int(os.getenv('PORT',5000))
     print("Starting app on port %d" %port)
     app.run(debug=True, port=port, host='0.0.0.0')
