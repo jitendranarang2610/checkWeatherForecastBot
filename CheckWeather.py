47aac528807c5be7a6d7fbeca87464aa491c34a0
jitendranarang2610 <jitendra.narang2610@gmail.com>
2021-08-15 14:27:53 +1000

    Update CheckWeather.py

--- CheckWeather.py
+++ CheckWeather.py
@@ -5,11 +5,15 @@ import requests
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask_cors import CORS, cross_origin
 
 # Flask app should start in global layout
 app = Flask (__name__)
+cors = CORS(app)
 
 @app.route('/webhook',methods=['POST'])
+@cross_origin()
+
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
